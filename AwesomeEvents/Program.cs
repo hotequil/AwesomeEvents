cbf9839e35982dcb4d949d84c21c9c45182d37dd
hotequil <hotequil@protonmail.com>
2023-11-27 13:49:43 -0300

    feat: connected to db

--- AwesomeEvents/Program.cs
+++ AwesomeEvents/Program.cs
@@ -1,13 +1,38 @@
 using AwesomeEvents.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("EventsCs");
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddSingleton<EventsDbContext>();
+builder.Services.AddDbContext<EventsDbContext>(options => options.UseSqlServer(connectionString));
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc(
+        "v1", 
+        new OpenApiInfo
+        {
+            Title = "AwesomeEvents",
+            Version = "v1",
+            Contact = new OpenApiContact
+            {
+                Name = "João Paulo Hotequil",
+                Email = "hotequil@protonmail.com",
+                Url = new Uri("https://hotequil.tech")
+            }
+        }
+    );
+
+    var xmlFile = "AwesomeEvents.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    
+    options.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
