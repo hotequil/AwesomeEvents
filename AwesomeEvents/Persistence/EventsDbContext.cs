7b74d46d931b2480a5d6b43cf3fe0e323dee7d69
hotequil <hotequil@protonmail.com>
2023-11-27 13:46:12 -0300

    feat: done DbContext

--- AwesomeEvents/Persistence/EventsDbContext.cs
+++ AwesomeEvents/Persistence/EventsDbContext.cs
@@ -1,13 +1,35 @@
 using AwesomeEvents.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace AwesomeEvents.Persistence;
 
-public class EventsDbContext
+public class EventsDbContext : DbContext
 {
-    public List<Event> Events { get; set; }
+    public DbSet<Event> Events { get; set; }
+    public DbSet<EventSpeaker> EventSpeakers { get; set; }
 
-    public EventsDbContext()
+    public EventsDbContext(DbContextOptions<EventsDbContext> options) : base(options) {}
+
+    protected override void OnModelCreating(ModelBuilder builder)
     {
-        Events = new List<Event>();
+        builder.Entity<Event>(entity =>
+        {
+            entity.HasKey(currentEvent => currentEvent.Id);
+
+            entity.Property(currentEvent => currentEvent.Title).IsRequired(false);
+
+            entity.Property(currentEvent => currentEvent.Description).HasMaxLength(200).HasColumnType("varchar(200)");
+
+            entity.Property(currentEvent => currentEvent.StartDate).HasColumnName("Start_Date");
+            
+            entity.Property(currentEvent => currentEvent.EndDate).HasColumnName("End_Date");
+
+            entity.HasMany(currentEvent => currentEvent.Speakers).WithOne().HasForeignKey(speaker => speaker.EventId);
+        });
+
+        builder.Entity<EventSpeaker>(entity =>
+        {
+            entity.HasKey(currentEvent => currentEvent.Id);
+        });
     }
 }
